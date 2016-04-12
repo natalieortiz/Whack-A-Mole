622c00cbfea099747bae05c26dd8e817252ade66
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-12 10:05:35 -0500

    Updated javascript.

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -2,9 +2,11 @@
 "use strict"
 $(document).ready(function(){
 	var hole;
-	var counter = 0;
-	var interval1;
-	var interval2;
+	var counter = 0; // keeps track of user score.
+	var interval1; //for first interval in gamePlay
+	var interval2;	//for second interval in gamePlay
+	var timer = 30;
+	var highscores = [];
 
 	//get random hole.  Holes are numbered 1 - 9.
 	function randomize (){
@@ -24,40 +26,58 @@ $(document).ready(function(){
 		console.log("remove dog");
 	}
 
-
+	//Start game button
 	$('#start').click(function(event){
+		var countdown = setInterval(updateTimer, 1000);
 		counter = 0;
 		gamePlay();
 	})
+
+	//Quit game button
+	$('#quit').click(function(event){
+    	removeWeenie();
+		stopPlay();
+	})
+
+    function updateTimer()
+    {
+        if (timer == 0) {
+        	stopPlay();
+			$('#timer').html(timer);
+        } else if (timer > 0) {
+            $('#timer').html(timer);
+        }
+        timer--;
+    }
 	
+	//Handles the game play using intervals. 
 	function gamePlay (){
+		var delay = 1000;
 		interval1 = setInterval(function(event){
 			removeWeenie();
-		},2000);
+		},delay);
 		interval2 = setInterval(function(event){
 			randomize();
 			addWeenie();
-		},3000);
+			console.log(timer);
+			delay = delay - 100;
+		},delay*1.5);
+		//Listens for user clicks and counts and adds score. 
 		$('#area').click(function(event){
 			var target = $(event.target).attr('id');
 			if (target == hole){
 				counter++;
 				tallyScore(counter);
 			}
-			console.log(counter);
 		});
 	}
 
+	//To clear out the intervals running. 
 	function stopPlay () {
+		removeWeenie();
     	clearInterval(interval1);
     	clearInterval(interval2);
 	}
-
-	$('#quit').click(function(event){
-    	removeWeenie();
-		stopPlay();
-	})
-	
 
 	//Keeps track of your current streak.
 	function tallyScore(){
