8eb54d3032d6acaedd5e920ad247678de00be70d
Natalie Ortiz <ndnatalie@gmail.com>
2016-04-13 13:26:51 -0500

    Added more comments to better explain code.

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,41 +1,36 @@
 (function(){
 "use strict"
 $(document).ready(function(){
-	var hole;
-	var counter; // keeps track of user score.
+	var hole;	//hole that is randomly selected. 
+	var score; // keeps track of user score.
 	var interval1; //for first interval in flashDogs
 	var interval2;	//for second interval in flashDogs
-	var countdown;
-	var timer;
-	var highscore = 0;
+	var countdown; //countdown variable for timer. 
+	var timer;	//timer interval for each game.
+	var highscore = 0; //High score initializes at zero. 
 	var woof = new Audio('/audio/woof1.mp3');
 	var bark = new Audio('/audio/bark.wav');
 	var whistle = new Audio('/audio/whistle.wav');
 
 
-	//get random hole.  Holes are numbered 1 - 9.
+	//Get random hole.  Holes are numbered 1 - 9.
 	function randomize (){
 		hole = Math.floor(Math.random()* 9)+1;
 	}
 	
-	//add Weenie to random hole. 
+	//Add Weenie image to random hole. 
 	function addWeenie (){
 		$("#"+ hole).addClass("add_weenie");
 	}
 
-	//removes the weenie dog from the selected hole. 
+	//Removes the weenie dog from the selected hole. 
 	function removeWeenie(){
 		$("#"+ hole).removeClass("add_weenie");
 	}
 
 	//Start game button
 	$('#start').click(function(event){
-		timer = 30;
-		//activates game area for user click. 
-		$("#area").on("click", userPlay);
-		countdown = setInterval(updateTimer, 1000);
-		flashDogs();
-		$('#scoreKeeper').html(counter);
+		init();
 	})
 
 	//Quit game button
@@ -45,37 +40,53 @@ $(document).ready(function(){
 		$("#area").off("click", userPlay);
 	})
 
+	//Initializes game. Resets values and turns "on" game event listener. 
+	function init (){
+		timer = 30;
+		//activates game area for user click. 
+		$("#area").on("click", userPlay);
+		//starts Timer countdown. 
+		countdown = setInterval(updateTimer, 1000);
+		flashDogs();
+		$('#scoreKeeper').html(score);	
+	}
+
+
 	//Countdown timer.
     function updateTimer()
     {	
+    	//Checks timer.  
         if (timer == 0) {
+        	//When time runs out, quit game. 
         	stopPlay();
 			$('#timer').html(timer);
 			$("#area").off("click", userPlay);
 			whistle.play();
-			if (counter > highscore){
-				$('#high_score').html(counter);
-				//shows video for high score. 
+			//Checking for new high score. 
+			if (score > highscore){
+				$('#high_score').html(score);
+				//Shows video for high score. 
 				$("#dogModal").modal("show");
 			}
+			//As time is running, do this: 
         } else if (timer > 0) {
             $('#timer').html(timer);
         }
         timer--;
     }
 	
-	//Handles the game play using intervals. 
+	//Flashes the images of dogs in the holes.  
 	function flashDogs (){
 		var delay = 700;
-		counter = 0;
+		score = 0;
 		interval1 = setInterval(function(event){
 			removeWeenie();
-		},delay);
+		}, delay);
 
 		interval2 = setInterval(function(event){
 			randomize();
 			addWeenie();
-		},delay*1.5);
+		}, delay*1.5);
 	}
 
 	//Compares user clicks with randomly selected hole. 
@@ -83,15 +94,14 @@ $(document).ready(function(){
 		var target = $(event.target).attr('id');
 		if (target == hole){
 			bark.play();
-			counter++;
-			console.log(counter);
+			score++;
 			tallyScore();
 		} else {
 			woof.play();	
 		}
 	}
 
-	//To clear out the intervals running. 
+	//To clear out the intervals running during . 
 	function stopPlay () {
 		removeWeenie();
     	clearInterval(interval1);
@@ -101,7 +111,7 @@ $(document).ready(function(){
 
 	//Keeps track of your current streak.
 	function tallyScore(){
-		$('#scoreKeeper').html(counter);
+		$('#scoreKeeper').html(score);
 	}
 
 
